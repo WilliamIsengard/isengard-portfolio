28807f03af5543f7d50c9f49db1fbf9f300af5de
William Isengard <william.isengard@gmail.com>
2019-01-15 16:23:46 -0700

    Created public_html folder and milestone-1 subfolder. Created milestone-1.php and added html boilerplate structure.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>William Isengard Web Developer Portfolio</title>
+	</head>
+	<body>
+		<h1>William Isengard- Web Developer</h1>
+	</body>
+</html>
