a8306c6cc1199ad0400f359ff4accbe02b863174
William Isengard <william.isengard@gmail.com>
2019-01-15 16:46:05 -0700

    Added <strong> element for main categories.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,21 +9,21 @@
 			<h2>PWP Milestone 1</h2>
 				<h3>Purpose, Audience, and Goal</h3>
 					<div>
-						<p>Purpose: </p>
-						<p>Audience: </p>
-						<p>Goal: </p>
+						<p><strong>Purpose:</strong> Awesome portfolio site.</p>
+						<p><strong>Audience:</strong> Hiring managers.</p>
+						<p><strong>Goal:</strong> Find a job!</p>
 					</div>
 				<h3>Persona</h3>
 					<div>
-						<p>Name: </p>
-						<p>Age: </p>
-						<p>Job: </p>
-						<p>Role: </p>
-						<p>Technology: </p>
-						<p>Behaviors and Attitude: </p>
-						<p>Frustrations and Needs: </p>
-						<p>User Story: </p>
-						<p>Goals: </p>
+						<p><strong>Name:<strong> John Doe</p>
+						<p><strong>Age:</strong> 34 </p>
+						<p><strong>Job:</strong> CTO at Meow Wolf </p>
+						<p><strong>Role:</strong> Oversees technology strategy and manages team responsible for web presence of company.</p>
+						<p><strong>Technology:</strong> Windows 10 with Firefox desktop PC and Samsung Galaxy S9 Android with Chrome mobile device.</p>
+						<p><strong>Behaviors and Attitude:</strong> Insert data here.</p>
+						<p><strong>Frustrations and Needs:</strong> Too much work and woo little time.</p>
+						<p><strong>User Story:</strong> As a user, I need to be able to find outstanding talent to fill an entry-level front-end Web Developer position.</p>
+						<p><strong>Goals:</strong> Find someone awesome.</p>
 					</div>
 	</body>
 </html>
