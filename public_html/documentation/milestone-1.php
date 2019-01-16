eb7fb05a5833042548e8f1a61beb972964c9a4ce
William Isengard <william.isengard@gmail.com>
2019-01-16 16:34:15 -0700

    Updated persona text.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,8 +10,8 @@
 				<h3>Purpose, Audience, and Goal</h3>
 					<ul>
 						<li><strong>Purpose:</strong> Create a compelling portfolio site that showcases the skills I have developed.</li>
-						<li><strong>Audience:</strong> Hiring managers at local software development companies.</li>
-						<li><strong>Goal:</strong> Get hired as an entry-level software developer at a local progressive company.</li>
+						<li><strong>Audience:</strong> Hiring managers at local software development companies and software developer peers that could assist with networking.</li>
+						<li><strong>Goal:</strong> Get hired as a full-time junior-level software developer at a local progressive company.</li>
 					</ul>
 				<h3>Persona</h3>
 					<img src="https://media.licdn.com/dms/image/C5603AQFaLssoFspx9A/profile-displayphoto-shrink_800_800/0?e=1553126400&v=beta&t=xIXh3zqel15eIOE75bbAOxSTy0lXSCkgOAR86dseq3g" alt="Corvas Brinkerhoff" style="width:250px;height:250px;"/>
@@ -20,8 +20,8 @@
 						<li><strong>Age:</strong> 34 </li>
 						<li><strong>Location:</strong> Santa Fe, NM</li>
 						<li><strong>Occupation:</strong> Cofounder and SVP of Experience Design at Meow Wolf </li>
-						<li><strong>Role:</strong>Primary responsibilities include collaboration and team building with the goal of steering the immersive experiences at Meow Wolf towards dynamic, multimedia-rich, and interactive environments.</li>
-						<li><strong>Education:</strong>Columbus College of Art & Design</li>
+						<li><strong>Role:</strong> Primary responsibilities include collaboration and team building with the goal of steering the immersive experiences at Meow Wolf towards dynamic, multimedia-rich, and interactive environments.</li>
+						<li><strong>Education:</strong> Columbus College of Art & Design</li>
 						<li><strong>Technology:</strong>
 							<ul>
 								<li><em>Laptop:</em> 15" HP Envy.</li>
@@ -31,10 +31,10 @@
 								<li><em>Mobile Browser:</em> Chrome.</li>
 							</ul>
 						</li>
-						<li><strong>Behaviors and Attitude:</strong> Corvas is extremely active in the local art and tech communities, but does not engage frequently on professional networking sites.</li>
-						<li><strong>Frustrations and Needs:</strong> With three new locations in the works, Corvas has an ever-increasing workload and imperitive to grow his team with quality professionals. Meow Wolf is a high-profile employeer that generates high volumes of job applicants, but sorting through the masses is time-consuming and difficult.</li>
-						<li><strong>User Story:</strong> As a user, I need to be able to find outstanding talent to fill an entry-level front-end Web Developer position.</li>
-						<li><strong>Goals:</strong> Find someone awesome.</li>
+						<li><strong>Behaviors and Attitude:</strong> Corvas is extremly busy with time-sensitive projects and any free time is spent on moon-shot concepts. He is active in the local and national art and tech communities, but does not social network frequently. He skims resumes and rarely pursues unsolistated applicants and instead focuses on peer recommendations for new hires.</li>
+						<li><strong>Frustrations and Needs:</strong> With three new locations in the works around the country, Corvas has an ever-increasing workload and imperative to grow his team with quality professionals. Meow Wolf is a high-profile employeer that generates high volumes of job applicants, but sorting through the masses of resumes is time-consuming and difficult.</li>
+						<li><strong>User Story:</strong> As a user, I need to find a skilled, passionate, and self-actualized individual to fill an opening for a junior web developer position.</li>
+						<li><strong>Goals:</strong> Corvas is focused on building his team of skilled web developers quickly. The new locations located outside of New Mexico requires talent that can work indepently, but can collaborate with a team spread across multiple geographic locations. Corvas does not have time to scrutinize each application or resume and needs to quickly evaluate a candidate's portfolio easily and efficiently to determine if they are worth reviewing further.</li>
 					</ul>
 	</body>
 </html>
