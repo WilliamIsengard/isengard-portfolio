95af6459cacae13a649ab44870bc43095720ef83
William Isengard <william.isengard@gmail.com>
2019-01-16 16:10:21 -0700

    Changed paragraphs to unordered lists and added text.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -13,17 +13,28 @@
 						<p><strong>Audience:</strong> Hiring managers.</p>
 						<p><strong>Goal:</strong> Find a job!</p>
 					</div>
-					<p></p>
-					<p></p>
+				<p></p>
+				<p></p>
 				<h3>Persona</h3>
 					<div>
-						<p><strong>Name:</strong> John Doe</p>
-						<p><strong>Age:</strong> 34 </p>
-						<p><strong>Job:</strong> CTO at Meow Wolf </p>
-						<p><strong>Role:</strong> Oversees technology strategy and manages team responsible for web presence of company.</p>
-						<p><strong>Technology:</strong> Windows 10 with Firefox desktop PC and Samsung Galaxy S9 Android with Chrome mobile device.</p>
-						<p><strong>Behaviors and Attitude:</strong> Insert data here.</p>
-						<p><strong>Frustrations and Needs:</strong> Too much work and woo little time.</p>
+						<ul>
+							<li><strong>Name:</strong> Corvas Brinkerhoff</li>
+							<li><strong>Age:</strong> 34 </li>
+							<li><strong>Location:</strong> Santa Fe, NM</li>
+							<li><strong>Occupation:</strong> Cofounder and SVP of Experience Design at Meow Wolf </li>
+							<li><strong>Role:</strong>Primary responsibilities include collaboration and team building with the goal of steering the immersive experiences at Meow Wolf towards dynamic, multimedia-rich, and interactive environments.</li>
+							<li><strong>Education:</strong>Columbus College of Art & Design</li>
+							<li><strong>Technology:</strong>
+								<ul>
+									<li><em>Laptop:</em> 15" HP Envy.</li>
+									<li><em>OS:</em> Windows 10 Pro.</li>
+									<li><em>Browser:</em> Chrome.</li>
+									<li><em>Phone:</em> Samsung Galaxy S8. </li>
+									<li><em>Mobile Browser:</em> Chrome.</li>
+								</ul>
+							</li>
+						<p><strong>Behaviors and Attitude:</strong> Corvas is extremely active in the local art and tech communities, but does not engage frequently on professional networking sites.</p>
+						<p><strong>Frustrations and Needs:</strong> With three new locations in the works, Corvas has an ever-increasing workload and imperitive to grow his team with quality professionals. Meow Wolf is a high-profile employeer that generates high volumes of job applicants, but sorting through the masses is time-consuming and difficult.</p>
 						<p><strong>User Story:</strong> As a user, I need to be able to find outstanding talent to fill an entry-level front-end Web Developer position.</p>
 						<p><strong>Goals:</strong> Find someone awesome.</p>
 					</div>
