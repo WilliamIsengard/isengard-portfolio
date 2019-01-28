6c0b6b30fd8dc901aa31aa243df55036cdc31623
William Isengard <william.isengard@gmail.com>
2019-01-28 09:29:23 -0700

    Created milestone-2.php file and added html framework

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>William Isengard Web Developer Portfolio- Content Strategy</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+			<strong>Who Am I?: </strong>A brief one-line introduction that clearly states my name and role as a Front End Developer. This welcome message will be overlaid on a large background image of my home office setup.
+			<p></p>
+			<strong>What Do I Do?: </strong>This section will detail my professional history, core skill set, and professional highlights. This section will showcase my strengths and personality. I will feature a contrasting sidebar that lists my core skills and programming languages which will be easily readable at a glance. The tone of voice will be personable and casual, as I am only interested in opportunities with individuals and organizations that I am a good personality fit for.
+			<p></p>
+			<strong>Can I Help You?: </strong>I want this to be an animated, responsive image gallery containing 4-6 examples of my best work. Gallery pop-up will include my role and contributions to each project, with iconography to represent the technology used.
+			<p></p>
+			<strong>How Can I Be Reached?: </strong>This section will contain a simple contact form, my professional email address, and additional links to my professional social media profiles: LinkedIn, Twitter, GitHub, Dribbble. (These will also be linked to in the footer site wide via icons.)
+		<h1>Wireframes</h1>
+	</body>
+</html>
+
