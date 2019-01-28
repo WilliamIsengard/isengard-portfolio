3a50dca0d751798e1750cecb94aa3ab0004f749f
William Isengard <william.isengard@gmail.com>
2019-01-28 14:35:13 -0700

    Updated text for content strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,13 +6,13 @@
 	</head>
 	<body>
 		<h1>Content Strategy</h1>
-			<strong>Who Am I?: </strong>A brief one-line introduction that clearly states my name and role as a Front End Developer. This welcome message will be overlaid on a large background image of my home office setup.
+			<strong>Who Am I?: </strong>A brief and personable introduction about myself as a full stack web developer and as a person. The images will represent key elements of myself (tentatively my family, CNM Bootcamp, and UNM alumni). The header will include a headshot and personalized background.
 			<p></p>
-			<strong>What Do I Do?: </strong>This section will detail my professional history, core skill set, and professional highlights. This section will showcase my strengths and personality. I will feature a contrasting sidebar that lists my core skills and programming languages which will be easily readable at a glance. The tone of voice will be personable and casual, as I am only interested in opportunities with individuals and organizations that I am a good personality fit for.
+			<strong>What Do I Do?: </strong>This section will include my developer skillset and professional accomplishments. A scrolling gallery will include brand logos of my professional highlights that expand with textual details when clicked (i.e. NM Solar Group image expands to include details on position and achievements). On mobile browsers, the header from the first section will reduce in size and stay static in this and subsequent sections. On full screen browsers, the header will stay static at top of page and the menu buttons will contextually change color when user is on the relevent section of the page.
 			<p></p>
-			<strong>Can I Help You?: </strong>I want this to be an animated, responsive image gallery containing 4-6 examples of my best work. Gallery pop-up will include my role and contributions to each project, with iconography to represent the technology used.
+			<strong>Can I Help You?: </strong>This section will provide a basic synopsis on how I can help a potential client. It will also include a contact form (simplified on mobile and more detailed on full screen browsers).
 			<p></p>
-			<strong>How Can I Be Reached?: </strong>This section will contain a simple contact form, my professional email address, and additional links to my professional social media profiles: LinkedIn, Twitter, GitHub, Dribbble. (These will also be linked to in the footer site wide via icons.)
+			<strong>How Can I Be Reached?: </strong>The closing section will include ways I can be reached via my professional email address, phone number, LinkedIn page, and GitHub repository.
 		<h1>Wireframes</h1>
 			<h2>Mobile Layout</h2>
 				<img src= "who-am-i-wireframe.png" alt="Who Am I? Wireframe"/>
@@ -23,4 +23,3 @@
 				<img src= "full-screen-wireframe.png" alt="Full Screen Wireframe"/>
 	</body>
 </html>
-
